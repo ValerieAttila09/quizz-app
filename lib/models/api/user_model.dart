fc316fe7d4297c1c610fa3be00eb5c1da6207995
valerieattilaalfath <valerieattilaalfath@gmail.com>
2025-12-14 12:47:13 +0000

    changed feature using json data instead of cloud database

--- lib/models/api/user_model.dart
+++ lib/models/api/user_model.dart
@@ -1,39 +1,20 @@
-class UserModel {
-  final String id;
-  final String email;
+class User {
   final String username;
-  final String fullName;
-  final DateTime createdAt;
-  final DateTime updatedAt;
+  final String password;
 
-  UserModel({
-    required this.id,
-    required this.email,
-    required this.username,
-    required this.fullName,
-    required this.createdAt,
-    required this.updatedAt,
-  });
+  User({required this.username, required this.password});
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'] as String? ?? '',
-      email: json['email'] as String? ?? '',
-      username: json['username'] as String? ?? '',
-      fullName: json['fullName'] as String? ?? '',
-      createdAt: DateTime.tryParse(json['createdAt'] as String? ?? '') ?? DateTime.now(),
-      updatedAt: DateTime.tryParse(json['updatedAt'] as String? ?? '') ?? DateTime.now(),
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      username: json['username'],
+      password: json['password'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'email': email,
       'username': username,
-      'fullName': fullName,
-      'createdAt': createdAt.toIso8601String(),
-      'updatedAt': updatedAt.toIso8601String(),
+      'password': password,
     };
   }
 }
